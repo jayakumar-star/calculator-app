b4e354cc5a321136677399e32e4bca3b908dfa53
jayakumar <hjayakumar519@gmail.com>
2025-01-08 06:26:15 -0800

    first version of calculator

---
+++ arithmetic operator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Author : Jayakumar
+#Date : 29-12-2024
+#Description : Arithmetic operators
+
+
+read -p " Enter first number : " n1
+read -p " enter second number : " n2
+
+echo the sum of two numbers is $((n1+n2))
+
+
+echo the differnce of two numbers is $((n1-n2))
+
+
+echo the product of two numbers is $((n1*n2))
+
+
+echo the division of two numbers is $((n1/n2))
+
+
+
+
